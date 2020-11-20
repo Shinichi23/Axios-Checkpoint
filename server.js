a01b7fb11a9dc5490044ade73f9ebb80a60cd2d9
Taha Chaoui <taha-chaoui@hotmail.com>
2020-11-21 00:04:01 +0100

    Finished

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const axios = require("axios");
 const app = express();
 const port = 4000;
 
-app.listen(port, () => {
-  console.log(`server is running at ${port}`);
-});
-
-const url =
-  "api.openweathermap.org/data/2.5/weather?q=kyoto&appid=7dff063d1af37b3761971574ddc4bbe3";
-
-app.get("/kyoto", (req, res, next) => {
+app.get("/:cityName", (req, res, next) => {
   axios
-    .get(url)
+    .get(
+      `api.openweathermap.org/data/2.5/weather?q=${req.params.cityName}&appid=7dff063d1af37b3761971574ddc4bbe3`
+    )
     .then((data) => {
       res.json(data.data);
     })
     .catch((err) => {
       console.log(err);
     });
+});
+app.listen(port, () => {
+  console.log(`server is running at ${port}`);
 });
